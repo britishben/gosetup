cc3a46480797b5d7d0d4bf2c6aacc1942ca4a915
Benjamin P. McKenna <britishben@gmail.com>
2017-10-17 17:17:30 +0100

    Allowed recovery from failed download, and fixed my overquoting

--- gosetup.sh
+++ gosetup.sh
@@ -26,8 +26,8 @@ goversions=$( git ls-remote -t https://go.googlesource.com/go | cut -d/ -f3 | gr
 
 function goinstall() {
     mkdir -p $install/go-"$1"
-    wget -P $tmpdir https://storage.googleapis.com/golang/go"$1".linux-amd64.tar.gz
-    tar xvf $tmpdir/go"$1".linux-amd64.tar.gz -C "$install"/go-"$1" --strip-components=1
+    wget -c -P $tmpdir https://storage.googleapis.com/golang/go"$1".linux-amd64.tar.gz
+    tar x --keep-newer-files -f $tmpdir/go"$1".linux-amd64.tar.gz -C "$install"/go-"$1" --strip-components=1
     rm $tmpdir/go"$1".linux-amd64.tar.gz
     #whiptail --title "Success" --msgbox "Installed Go Version $1 to $install/go-$1" 15 30 
 }
@@ -39,7 +39,7 @@ done
 
 selected="$(whiptail --title "Versions"  --checklist --noitem --separate-output\
     "Select versions to install" 15 30 8 \
-    "$list" \
+    $list \
     3>&1 1>&2 2>&3)" 
 
 if [ "$?" != "0" ]; then exit; fi
@@ -51,7 +51,7 @@ for choice in $selected; do
 done
 
 chosen="$(whiptail --title "Version" --menu --noitem "Select version to symlink" 15 30 6 \
-    "$list" \
+    $list \
     3>&1 1>&2 2>&3)" 
 
 binaries="/usr/local/bin"
@@ -62,8 +62,8 @@ binaries="/usr/local/bin"
 #    "4" "$HOME/bin"  \
 #    3>&1 1>&2 2>&3)"
 
-echo ln -s "$install"/go-"$chosen" "$install"/go
-echo ln -s "$install"/go/bin/go "$install"/go/bin/godoc "$install"/go/bin/gofmt "$binaries"
+ln -s "$install"/go-"$chosen" "$install"/go
+ln -s "$install"/go/bin/go "$install"/go/bin/godoc "$install"/go/bin/gofmt "$binaries"
 
 if [ -x "$binaries"/go ]; then
     whiptail --title "Success" --msgbox "Symlinked $binaries/go to Go Version $chosen" 15 30 
